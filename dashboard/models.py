338bb5cef9b5197ad7c1fa1171c03ea07a072cbb
Freddy Garcia Abreu <buscando_alink@hotmail.com>
2017-07-14 09:34:17 -0400

    Add models and db connection

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class BaseModel(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
+
+class Indicator(BaseModel):
+    name = models.CharField(max_length=50)
+
+class IndicatorData(BaseModel):
+    load_date = models.DateField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+class IndicatorType(BaseModel):
+    name = models.CharField(max_length=50)
+    key = models.CharField(max_length=50)
+
+class Region(BaseModel):
+    name = models.CharField(max_length=50)
+    map_id = models.CharField(max_length=50)
+
+class Office(BaseModel):
+    name = models.CharField(max_length=50)
+    longitude = models.DecimalField(max_digits=10, decimal_places=8)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8)
+    code = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    region_map = models.CharField(max_length=50)
