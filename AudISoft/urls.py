333562b73e231cd753a8b8e5856c96b143d9ed71
Freddy Garcia Abreu <buscando_alink@hotmail.com>
2017-07-24 10:21:16 -0400

    Ädd html and static content

--- AudISoft/urls.py
+++ AudISoft/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from dashboard.urls import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^dashboard/', views.dashboard, name='dashboard'),
 ]
