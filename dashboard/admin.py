e1579b58557479f76c51003f151d51e612bad91a
Freddy Garcia Abreu <buscando_alink@hotmail.com>
2017-07-24 16:45:51 -0400

    Add models to django admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Indicator, Office, Region
 # Register your models here.
+
+admin.site.register(Indicator)
+admin.site.register(Office)
+admin.site.register(Region)
