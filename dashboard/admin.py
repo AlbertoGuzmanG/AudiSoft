c700e7ea21a83f12c65ae7b52d30f4b59130db87
Freddy Garcia Abreu <buscando_alink@hotmail.com>
2017-08-09 08:42:33 -0400

    Customize admin site

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Indicator, Office, Region
 # Register your models here.
 
+
+class RegionAdmin(admin.ModelAdmin):
+	fields = ('name',)
+
+
+
+class OfficeAdmin(admin.ModelAdmin):
+	fields = ('name',)
+
+
 admin.site.register(Indicator)
-admin.site.register(Office)
-admin.site.register(Region)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Office, OfficeAdmin)
